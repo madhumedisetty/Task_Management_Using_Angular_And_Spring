91c68153dea919fceb340be66c5a55a8c222280d
madhumedisetty <medisettymadhuri2002@gmail.com>
2024-11-14 18:24:49 +0530

    backend files

--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(classes = TaskManagementAppBackendApplication.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BackendApplicationTests {
 
 	@Test
